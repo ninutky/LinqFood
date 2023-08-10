99607b84babea9ab17b581b030f7d39006b93905
Nagyeong Park <zzum0701@naver.com>
2023-08-10 14:25:52 +0900

    데이터가 삭제, 수정, 추가가 되지 않도록 설정

--- LinqFood/Form1.Designer.cs
+++ LinqFood/Form1.Designer.cs
@@ -55,6 +55,7 @@ namespace LinqFood
             this.dataGridView1.DataSource = this.foodBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(13, 20);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(356, 557);
             this.dataGridView1.TabIndex = 0;
