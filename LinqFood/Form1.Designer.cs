1e637d7af4f7f8e6f9b3740601ea33f18f5a77a7
Nagyeong Park <zzum0701@naver.com>
2023-08-10 14:09:47 +0900

    Form 타이틀에 학번, 이름 추가

--- LinqFood/Form1.Designer.cs
+++ LinqFood/Form1.Designer.cs
@@ -143,7 +143,7 @@ namespace LinqFood
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "3105 박나경";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.foodBindingSource)).EndInit();
