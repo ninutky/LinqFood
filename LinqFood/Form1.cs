a0e99eafff91aa8dc6a8bb516c1df0f42354447d
Nagyeong Park <zzum0701@naver.com>
2023-08-10 13:37:56 +0900

    GUI 디자인 작업

--- LinqFood/Form1.cs
+++ LinqFood/Form1.cs
@@ -16,5 +16,30 @@ namespace LinqFood
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
